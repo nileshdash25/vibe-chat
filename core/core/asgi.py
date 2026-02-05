70196b6db6e3d4d31d0333340c0b9f55cd5b2e27
Nilesh Dash <nileshdash25@gmail.com>
2026-02-05 05:02:00 -0800

    Fix ASGI import path for nested chat app and resolve Daphne startup error

--- core/core/asgi.py
+++ core/core/asgi.py
@@ -1,16 +1,16 @@
 import os
-import django
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
-import chat.routing
+from core.chat import routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.core.settings")
-django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(chat.routing.websocket_urlpatterns)
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
     ),
 })
