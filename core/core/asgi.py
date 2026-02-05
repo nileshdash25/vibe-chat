8dadd8c8ead3b1351ea679728dfd75a507613388
Nilesh Dash <nileshdash25@gmail.com>
2026-02-05 04:54:45 -0800

    Fix Django settings module path for ASGI (core.core.settings)

--- core/core/asgi.py
+++ core/core/asgi.py
@@ -1,13 +1,12 @@
 import os
 import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-django.setup()
-
-from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
 import chat.routing
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.core.settings")
+django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
