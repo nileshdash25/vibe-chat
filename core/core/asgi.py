ef9766b3c1770cf99f5ce9b4bbec54f37fa4973e
Nilesh Dash <nileshdash25@gmail.com>
2026-02-05 04:22:10 -0800

    Fix ASGI configuration and correct Daphne start command for Railway deployment

--- core/core/asgi.py
+++ core/core/asgi.py
@@ -1,6 +1,17 @@
 import os
-from django.core.asgi import get_asgi_application
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django.setup()
 
-application = get_asgi_application()
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(chat.routing.websocket_urlpatterns)
+    ),
+})
