c66f9c879c5d774af02f453dd155364bb55d7f3e
Nilesh Dash <nileshdash25@gmail.com>
2026-02-07 07:28:56 -0800

    Force Allow All Hosts

--- core/core/settings.py
+++ core/core/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # --------------------------------------------------
 SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-change-this")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = ['*']
 
 # --------------------------------------------------
 # APPLICATIONS
